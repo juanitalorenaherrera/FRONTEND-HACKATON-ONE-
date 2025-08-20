81c1759db3c88fd91a9307420435361a1a5ddd5a
luifer991 <luifer991@gmail.com>
2025-08-19 19:38:36 -0500

    Add petService for managing pet data with API integration

---
+++ src/componentes/petService.tsx
@@ -0,0 +1,21 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:8080/pets';
+
+interface PetData {
+	name: string;
+	type: string;
+}
+
+export const addPet = async (token: string, pet: PetData) => {
+	const res = await axios.post(API_URL, pet, {
+		headers: { Authorization: `Bearer ${token}` },
+	});
+	return res.data;
+};
+
+export const getPets = async (token: string) => {
+	const res = await axios.get(API_URL, {
+		headers: { Authorization: `Bearer ${token}` },
+	});
+	return res.data;
+};
