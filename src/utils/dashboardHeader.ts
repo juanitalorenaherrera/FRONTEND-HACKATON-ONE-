fe80ee9e4b8696a50e2d9098d0ff2234cc3f8b17
luifer991 <luifer991@gmail.com>
2025-09-11 02:24:25 -0500

    refactor: move functions and change switch case to improve performance and readability

---
+++ src/utils/dashboardHeader.ts
@@ -0,0 +1,23 @@
+import { Role } from "../types/authStore";
+
+export const displayName = (role?: Role): string | undefined => {
+	if (role)
+		return Role.ADMIN === role
+			? 'Panel de Administrador'
+			: Role.SITTER === role
+			? 'Panel de Cuidador'
+			: Role.CLIENT === role
+			? 'Panel de Cliente'
+			: 'Dashboard';
+};
+
+export const getRoleColor = (role?: Role): string | undefined => {
+	if (role)
+		return Role.CLIENT === role
+			? 'from-orange-500 to-orange-600'
+			: Role.SITTER === role
+			? 'from-blue-500 to-blue-600'
+			: Role.ADMIN === role
+			? 'from-purple-500 to-purple-600'
+			: 'from-gray-500 to-gray-600';
+};
