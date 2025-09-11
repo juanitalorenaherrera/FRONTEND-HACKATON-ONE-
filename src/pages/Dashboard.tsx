86ced27e99b50d808cd32a4c1f2783d47102767e
luifer991 <luifer991@gmail.com>
2025-09-11 03:04:58 -0500

    readability

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,10 @@
 const Dashboard = () => {
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h2>Panel del Usuario</h2>
-      <p>Aquí podrás ver tus reservas y cuidadores.</p>
-    </div>
-  );
+	return (
+		<div style={{ padding: '2rem' }}>
+			<h2>Panel del Usuario</h2>
+			<p>Aquí podrás ver tus reservas y cuidadores.</p>
+		</div>
+	);
 };
 
 export default Dashboard;
