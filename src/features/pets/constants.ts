3f5467699f52a541a06c74c41308dbd77ba7eba9
luifer991 <luifer991@gmail.com>
2025-09-13 17:11:40 -0500

    filter with enum

--- src/features/pets/constants.ts
+++ src/features/pets/constants.ts
@@ -1,21 +1,22 @@
 // features/pets/constants.ts
 
 import type { PetFilters } from './types';
+import { SortBy, SortOrderBy } from './types';
 
 // Le damos el tipo correcto a las constantes
-const DEFAULT_SORT_BY: PetFilters['sortBy'] = 'name';
-const DEFAULT_SORT_ORDER: PetFilters['sortOrder'] = 'asc';
+const DEFAULT_SORT_BY: PetFilters['sortBy'] = SortBy.CREATED;
+const DEFAULT_SORT_ORDER: PetFilters['sortOrder'] = SortOrderBy.DESC;
 
 export const PETS_CONFIG = {
-    DEFAULT_SORT_BY,
-    DEFAULT_SORT_ORDER,
-    MAX_PETS_PER_PAGE: 12,
+	DEFAULT_SORT_BY,
+	DEFAULT_SORT_ORDER,
+	MAX_PETS_PER_PAGE: 12,
 };
 export const PET_SPECIES_OPTIONS = [
-    { label: 'Dog', value: 'dog' },
-    { label: 'Cat', value: 'cat' },
-    { label: 'Bird', value: 'bird' },
-    { label: 'Rabbit', value: 'rabbit' },
-    { label: 'Reptile', value: 'reptile' },
-    { label: 'Other', value: 'other' },
+	{ label: 'Dog', value: 'dog' },
+	{ label: 'Cat', value: 'cat' },
+	{ label: 'Bird', value: 'bird' },
+	{ label: 'Rabbit', value: 'rabbit' },
+	{ label: 'Reptile', value: 'reptile' },
+	{ label: 'Other', value: 'other' },
 ];
