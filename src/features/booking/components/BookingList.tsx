28272841d8ae5ddf133b2b6a8d33131564b72563
luifer991 <luifer991@gmail.com>
2025-09-16 09:17:21 -0500

    remove: unused react import

--- src/features/booking/components/BookingList.tsx
+++ src/features/booking/components/BookingList.tsx
@@ -2,18 +2,17 @@
 
 import { BookingCard } from './BookingCard';
 import type { BookingSummary } from '../types';
-import React from 'react';
 
 interface BookingsListProps {
-  bookings: BookingSummary[];
+	bookings: BookingSummary[];
 }
 
 export function BookingsList({ bookings }: BookingsListProps) {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {bookings.map((booking) => (
-        <BookingCard key={booking.id} booking={booking} />
-      ))}
-    </div>
-  );
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+			{bookings.map((booking) => (
+				<BookingCard key={booking.id} booking={booking} />
+			))}
+		</div>
+	);
 }
