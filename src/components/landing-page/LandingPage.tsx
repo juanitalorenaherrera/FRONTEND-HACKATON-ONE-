49d2a0043c701ec3644f71ec3ed85ae4bed6c527
luifer991 <luifer991@gmail.com>
2025-09-18 00:42:07 -0500

    add: another components and adding style in main app

--- src/components/landing-page/LandingPage.tsx
+++ src/components/landing-page/LandingPage.tsx
@@ -5,16 +5,20 @@ import Categories from './components/Categories';
 import Products from './components/Products';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
+import Services from './components/Services';
+import Contact from './components/Contact';
 
 export default function LandingPage(): React.ReactElement {
 	return (
-		<>
+		<main className='max-w-[1080px] m-auto'>
 			<HeaderLandingPage />
 			<Hero />
 			<Categories />
 			<Products />
+			<Services />
 			<Testimonials />
+			<Contact />
 			<Footer />
-		</>
+		</main>
 	);
 }
