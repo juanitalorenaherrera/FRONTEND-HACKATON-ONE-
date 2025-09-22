887ca0612fa01574525818e1f425ba0f8ea3d394
luifer991 <luifer991@gmail.com>
2025-09-22 11:24:38 -0500

    refactor: replace import for using alias

--- src/components/auth/RoleProtectedRoute.tsx
+++ src/components/auth/RoleProtectedRoute.tsx
@@ -1,25 +1,27 @@
 import { Navigate, Outlet } from 'react-router';
-import { useAuthStore } from '../../store/AuthStore';
-import type { Profile } from '../../types/authStore';
+import { useAuthStore } from '@/store/AuthStore';
+import type { Profile } from '@/features/auth/types/authStore';
 
 interface RoleProtectedRouteProps {
-    allowedRoles: Profile['role'][];
+	allowedRoles: Profile['role'][];
 }
 
-export default function RoleProtectedRoute({ allowedRoles }: RoleProtectedRouteProps) {
-    const user = useAuthStore((state) => state.profile);
+export default function RoleProtectedRoute({
+	allowedRoles,
+}: RoleProtectedRouteProps) {
+	const user = useAuthStore((state) => state.profile);
 
-    if (!user) {
-    // Si no hay usuario, redirige a la página de login
-    return <Navigate to="/login" replace />;
-    } 
+	if (!user) {
+		// Si no hay usuario, redirige a la página de login
+		return <Navigate to="/login" replace />;
+	}
 
-    if (!allowedRoles.includes(user.role)) {
-    // Si el rol del usuario no está permitido, redirige a la página de inicio
-    // Podrías redirigir a una página de "Acceso Denegado" también
-    return <Navigate to="/" replace />;
-    }
+	if (!allowedRoles.includes(user.role)) {
+		// Si el rol del usuario no está permitido, redirige a la página de inicio
+		// Podrías redirigir a una página de "Acceso Denegado" también
+		return <Navigate to="/" replace />;
+	}
 
-  // Si el usuario existe y su rol es válido, muestra el contenido de la ruta
-    return <Outlet />;
+	// Si el usuario existe y su rol es válido, muestra el contenido de la ruta
+	return <Outlet />;
 }
