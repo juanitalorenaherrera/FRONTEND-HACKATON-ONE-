b95f16c769a93b63e4baf5fd8d44da77f1c78f5f
luifer991 <luifer991@gmail.com>
2025-09-22 08:03:25 -0500

    add: is auth to protected router

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,15 +1,14 @@
-import { useAuthStore } from '../store/AuthStore';
+import { useAuthStore } from '@/store/AuthStore';
 import { Navigate, Outlet } from 'react-router';
 
 
 const ProtectedRoute = () => {
 	const user = useAuthStore((state) => state.profile);
+	const isAuth = useAuthStore((state) => state.isAuth);
 
-	if (!user) {
+	if (!user && !isAuth)
     	// Si no hay usuario, redirige a la página de login
 		return <Navigate to="/login" replace />;
-	}
-
   	// Si hay un usuario, renderiza el componente de la ruta (ej. OwnerDashboard)
 	return <Outlet />;
 };
