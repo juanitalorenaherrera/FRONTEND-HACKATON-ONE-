fdccb7e2eda23b2b7f194b11d9bcf4ccd6b44357
luifer991 <luifer991@gmail.com>
2025-09-25 16:48:15 -0500

    delete: move file to another directory for implementing screaming architecture

--- src/schemas/loginSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const loginSchema = z.object({
-	email: z.email('Ingresa un email válido'),
-	password: z.string().min(1, 'La contraseña es requerida'),
-});
-
-export type LoginFormData = z.infer<typeof loginSchema>;
